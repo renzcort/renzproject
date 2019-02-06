f49f6c0d958105782ca5ad156107f1b9b2077c1b
renzcort <kharenputra@gmail.com>
2019-02-06 16:52:26 +0700

    First commit

---
+++ application/controllers/admin/Home.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+
+  public function index()
+  {
+    $data['content'] = 'admin/home';
+    $this->load->view('admin/layout/_default', $data);
+  }
+
+}
+
+/* End of file Dashboard.php */
+/* Location: ./application/controllers/admin/Dashboard.php */
