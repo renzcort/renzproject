6ac5e5c40b00c1d53e1570d18b373ee067c58193
renzcort <kharenputra@gmail.com>
2019-03-08 17:55:44 +0700

    bootstrap

--- template/bootstrap-4/index.php
+++ template/bootstrap-4/index.php
@@ -17,7 +17,7 @@
         <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
+          <ul class="navbar-nav ml-auto">
             <li class="nav-item active">
               <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
             </li>
@@ -50,7 +50,21 @@
     <div class="wraper">
       <div class="row">
         <div class="col-3 main-sidebar">
-          
+          <div class="profil">
+            <div class="card mb-3" style="max-width: 540px;">
+              <div class="row no-gutters">
+                <div class="col-md-4">
+                  <img src="http://dummyimage.com/800x600/4d494d/686a82.gif&text=placeholder+image" alt="placeholder+image" class="card-img rounded-circle">
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
         <div class="col-9 content-wraper">
           <div class="container">
