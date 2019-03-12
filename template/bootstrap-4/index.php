c51c5a606875835e7e70cb3bd1bce4c756d6e597
Rendi <renzcoding@gmail.com>
2019-03-12 07:37:37 +0700

    bootstrap

--- template/bootstrap-4/index.php
+++ template/bootstrap-4/index.php
@@ -47,103 +47,107 @@
       </nav>
     </header>
 
-
-    <main class="wraper" role="main">
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-2 sidebar overflow-hidden">
-            <div class="main-sidebar position-sticky">
-              <div class="user-panel d-flex flex-row">
-                <div class="image p-2 bd-highlight">
-                  <img src="http://dummyimage.com/800x600/4d494d/686a82.gif&text=placeholder+image" alt="placeholder+image" class="rounded-circle">
-                </div>
-                <div class="info p-2 bd-highlight">
-                  <p>Admin</p>
-                  <a href="#" class="pr-3 mt-3"><i class="fa fa-circle text-success"></i> Online</a>
-                </div>
+    <main class="wraper container-fluid" role="main">
+      <div class="row">
+        <div class="col-2 sidebar overflow-hidden">
+          <div class="main-sidebar overflow-hidden overflow-auto">
+            <div class="user-panel d-flex flex-row">
+              <div class="image p-2 bd-highlight">
+                <img src="http://dummyimage.com/800x600/4d494d/686a82.gif&text=placeholder+image" alt="placeholder+image" class="rounded-circle">
               </div>
-              <div class="menu">
-                <ul class="nav flex-column mt-2">
-                  <li class="nav-item">
-                    <a class="nav-link active" href="#">Active</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                  </li>
-                </ul>
-                <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-                  <span>Saved reports</span>
-                  <a class="d-flex align-items-center text-muted" href="#">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus-circle"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg>
-                  </a>
-                </h6>
-                <ul class="nav flex-column">
-                  <li class="nav-item">
-                    <a class="nav-link active" href="#">Active</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                  </li>
-                </ul>
+              <div class="info p-2 bd-highlight">
+                <p>Admin</p>
+                <a href="#" class="pr-3 mt-3"><i class="fa fa-circle text-success"></i> Online</a>
               </div>
+            </div>
+            <div class="menu">
+              <ul class="nav flex-column mt-2">
+                <li class="nav-item">
+                  <a class="nav-link active" href="#">Active</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                </li>
+              </ul>
+              <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+                <span>Saved reports</span>
+                <a class="d-flex align-items-center text-muted" href="#">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus-circle"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg>
+                </a>
+              </h6>
+              <ul class="nav flex-column">
+                <li class="nav-item">
+                  <a class="nav-link active" href="#">Active</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                </li>
+              </ul>
             </div>
           </div>
-          <div class="col-10 content p-0 ml-sm-auto col-lg-10 px-0 mt-5">
-            <div class="breadcrumb-content container pb-0">
-              <nav aria-label="breadcrumb">
-                <ol class="breadcrumb">
-                  <li class="breadcrumb-item"><a href="#">Home</a></li>
-                  <li class="breadcrumb-item"><a href="#">Library</a></li>
-                  <li class="breadcrumb-item active" aria-current="page">Data</li>
-                </ol>
-              </nav>
-            </div>
-
-            <div class="main container pt-2">
-              <div class="header d-flex justify-content-between align-items-center border-bottom">
-                <h1 class="font-weight-normal">Dashboard</h1>
-                <div class="btn-toolbar mb-2">
-                  <div class="btn-group mr-2">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
-                  </div>
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>                  
+        </div>
+        <div class="col-10 content p-0 ml-sm-auto col-lg-10 px-0 mt-5">
+          <div class="breadcrumb-content container pb-0">
+            <nav aria-label="breadcrumb">
+              <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                <li class="breadcrumb-item"><a href="#">Library</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Data</li>
+              </ol>
+            </nav>
+          </div>
+          <div class="main container pt-2">
+            <div class="header d-flex justify-content-between align-items-center border-bottom">
+              <h1 class="font-weight-normal">Dashboard</h1>
+              <div class="btn-toolbar mb-2">
+                <div class="btn-group mr-2">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
                 </div>
+                <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>                  
               </div>
-              <div class="main-content mt-4 mb-3">
-                <h4 class="font-weight-light pt-4 pb-2">System</h4>
-                <ul class="d-flex flex-row mb-4 pb-4 border-bottom list-unstyled">
-                  <li class="p-2 bd-highlight">General</li>
-                  <li class="p-2 bd-highlight">Routes</li>
-                  <li class="p-2 bd-highlight">Users</li>
-                  <li class="p-2 bd-highlight">Email</li>
-                  <li class="p-2 bd-highlight">Plugins</li>
-                </ul>
-                <h4 class="font-weight-light pt-4 pb-2">Content</h4>
-                <ul class="d-flex flex-row mb-4 pb-4 border-bottom list-unstyled">
-                  <li class="p-2 bd-highlight">Fields</li>
-                  <li class="p-2 bd-highlight">Sections</li>
-                  <li class="p-2 bd-highlight">Assets</li>
-                  <li class="p-2 bd-highlight">Globals</li>
-                  <li class="p-2 bd-highlight">Categories</li>
-                  <li class="p-2 bd-highlight">Tags</li>
-                  <li class="p-2 bd-highlight">Locales</li>
-                </ul>
-              </div>
+            </div>
+            <div class="main-content mt-4 mb-3">
+              <h4 class="font-weight-light pt-4 pb-2">System</h4>
+              <ul class="d-flex flex-row mb-4 pb-4 border-bottom list-unstyled">
+                <li class="p-2 bd-highlight">General</li>
+                <li class="p-2 bd-highlight">Routes</li>
+                <li class="p-2 bd-highlight">Users</li>
+                <li class="p-2 bd-highlight">Email</li>
+                <li class="p-2 bd-highlight">Plugins</li>
+              </ul>
+              <h4 class="font-weight-light pt-4 pb-2">Content</h4>
+              <ul class="d-flex flex-row mb-4 pb-4 border-bottom list-unstyled">
+                <li class="p-2 bd-highlight">Fields</li>
+                <li class="p-2 bd-highlight">Sections</li>
+                <li class="p-2 bd-highlight">Assets</li>
+                <li class="p-2 bd-highlight">Globals</li>
+                <li class="p-2 bd-highlight">Categories</li>
+                <li class="p-2 bd-highlight">Tags</li>
+                <li class="p-2 bd-highlight">Locales</li>
+              </ul>
             </div>
           </div>
+          <footer class="my-5 pt-5 text-muted text-center text-small">
+            <p class="mb-1">© 2017-2019 Company Name</p>
+            <ul class="list-inline">
+              <li class="list-inline-item"><a href="#">Privacy</a></li>
+              <li class="list-inline-item"><a href="#">Terms</a></li>
+              <li class="list-inline-item"><a href="#">Support</a></li>
+            </ul>
+          </footer>
         </div>
       </div>
     </main>
